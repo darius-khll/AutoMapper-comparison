c5e4d60d02830feaebf13e76ba4f52f34395509a
Ali Khalili <ali.khll.1994@gmail.com>
2017-01-03 17:13:59 +0330

    comment addtional codes

--- AutoMapperComparison/Program.cs
+++ AutoMapperComparison/Program.cs
@@ -23,28 +23,28 @@ namespace AutoMapperComparison
                 db.Database.Initialize(force: true);
                 db.Database.ExecuteSqlCommand("DBCC DROPCLEANBUFFERS"); //Removes all clean buffers from the buffer pool, and columnstore objects from the columnstore object pool
                 Console.WriteLine(db.Addresses.ProjectTo<AddressDto>());
-                Console.WriteLine(db.Addresses);
-                Console.WriteLine(db.Addresses.Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId }));
+                //Console.WriteLine(db.Addresses);
+                Console.WriteLine(db.Addresses.Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId, UserName = add.User.Name }));
             }
 
-            Console.WriteLine($"Normal Exec {DateTimeOffset.UtcNow}");
-            using (AppDbContext db = new AppDbContext())
-            {
-                db.Database.ExecuteSqlCommand("DBCC DROPCLEANBUFFERS");
-                Stopwatch watch = Stopwatch.StartNew();
-                List<Address> addresses = db.Addresses.AsNoTracking().ToList();
-                List<Address> addresses2 = db.Addresses.AsNoTracking().ToList();
-                watch.Stop();
-                Console.WriteLine($"{watch.ElapsedMilliseconds} {addresses.Count} {addresses2.Count}");
-            }
+            //Console.WriteLine($"Normal Exec {DateTimeOffset.UtcNow}");
+            //using (AppDbContext db = new AppDbContext())
+            //{
+            //    db.Database.ExecuteSqlCommand("DBCC DROPCLEANBUFFERS");
+            //    Stopwatch watch = Stopwatch.StartNew();
+            //    List<Address> addresses = db.Addresses.AsNoTracking().ToList();
+            //    List<Address> addresses2 = db.Addresses.AsNoTracking().ToList();
+            //    watch.Stop();
+            //    Console.WriteLine($"{watch.ElapsedMilliseconds} {addresses.Count} {addresses2.Count}");
+            //}
 
             Console.WriteLine($"Normal Select {DateTimeOffset.UtcNow}");
             using (AppDbContext db = new AppDbContext())
             {
                 db.Database.ExecuteSqlCommand("DBCC DROPCLEANBUFFERS");
                 Stopwatch watch = Stopwatch.StartNew();
-                List<AddressDto> addresses = db.Addresses.AsNoTracking().Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId }).ToList();
-                List<AddressDto> addresses2 = db.Addresses.AsNoTracking().Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId }).ToList();
+                List<AddressDto> addresses = db.Addresses.AsNoTracking().Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId, UserName = add.User.Name }).ToList();
+                List<AddressDto> addresses2 = db.Addresses.AsNoTracking().Select(add => new AddressDto { Id = add.Id, Code = add.Code, Title = add.Title, UserId = add.UserId, UserName = add.User.Name }).ToList();
                 watch.Stop();
                 Console.WriteLine($"{watch.ElapsedMilliseconds} {addresses.Count} {addresses2.Count}");
             }
